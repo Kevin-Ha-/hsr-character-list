0f1ac33e6d981d0ca21e801ccf5baf3b45e63c6a
Kevin Ha <kevin.james.ha@gmail.com>
2025-05-08 14:18:18 -0700

    cleanup and added images

---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+type characterData = {
+    name: string
+    thumb_src: string
+    full_src: string
+    rarity: number
+    element: string
+    element_src: string
+    character_path: string
+  }
