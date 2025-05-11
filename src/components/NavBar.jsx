8d4d441df827bfb5729556d2d721ceaaecda6708
Kevin Ha <kevin.james.ha@gmail.com>
2025-05-11 09:25:02 -0700

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,19 @@
-import styles from './NavBar.css'
+import styles from './NavBar.module.css'
 
 export default function() {
     return(
-        <header className="headerInfo"></header>
+        <header className={styles.headerInfo}>
+            <img className={styles.logo} src={require(`../images/logo.gif`)}/>
+            <section className={styles.navHeaderInfo}>
+                <img className={styles.dataBankIcon} src={require(`../images/dataBank.png`)}/>
+                <div className={styles.dataBankTextContainer}>
+                    <p className={styles.dataBankText}>Data Bank</p>
+                    <p style={{color: 'white', fontSize: '1.3em', margin: '0', fontWeight: 'bold'}}>Characters</p>
+                </div>
+            </section>
+            <section className={styles.navRightInfo}>
+                <p>Indexed</p>
+            </section>
+        </header>
     )
 }
